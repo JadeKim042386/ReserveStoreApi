90ccb11077bb98162bbcb4d742d4f3cf4a4f8c6d
JadeKim042386 <jk042386@gmail.com>
2024-01-25 15:44:40 +0900

    feat: defined jpa repositories - added Member, Reservation, Review, Store jpa repository - updated application.yml (datasource) #3

---
+++ src/main/java/org/zerobase/reservestoreapi/repository/StoreRepository.java
@@ -0,0 +1,7 @@
+package org.zerobase.reservestoreapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerobase.reservestoreapi.domain.Store;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
