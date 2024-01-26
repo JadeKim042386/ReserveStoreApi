96dcd50c790f5682735ff90117e2c211134dca8a
JadeKim042386 <jk042386@gmail.com>
2024-01-26 15:54:09 +0900

    refactor: integrate auditing fields into AuditingFields

--- src/main/java/org/zerobase/reservestoreapi/domain/auditing/AuditingFields.java
+++ src/main/java/org/zerobase/reservestoreapi/domain/auditing/AuditingFields.java
@@ -3,15 +3,29 @@ package org.zerobase.reservestoreapi.domain.auditing;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @Getter
 @ToString
 @MappedSuperclass
-public abstract class AuditingFields extends AuditingAt {
+public abstract class AuditingFields {
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected LocalDateTime modifiedAt;
+
     @CreatedBy
     @Column(nullable = false, updatable = false)
     protected String createdBy;
